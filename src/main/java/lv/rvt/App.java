a50b66504df5817f0a85c16887958a1e1fee6925
23DP3AKani <166131758+23DP3AKani@users.noreply.github.com>
2024-09-06 11:30:51 +0000

    Uzdevums

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,42 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        // Comment
+
+        // VScode keybind: Ctrl + /
+        
+        // Scanner scanner = new Scanner(System.in);
+        
+        // String input = scanner.nextLine();
+
+        // System.out.println("Sveiks " + input + "!");
+
+        // String name = "Alex";
+        // int age = 20;
+        // boolean isOnline = true;
+
+        // System.out.println("Once upon a time");
+        // System.out.println("There was");
+        // System.out.println("a program");
+        // System.out.println(name, age, isOnline);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ieavadi savu vārdu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ieavdi savu uzvārdu: ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Ieavadi savu grupu: ");
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + " " + name + " " +  lastname + " grupa: " + group);
 
     }
 
