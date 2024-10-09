34952e2e480edeea87ec486fc026896144f1bf79
23DP3AKani <166131758+23DP3AKani@users.noreply.github.com>
2024-10-09 06:42:14 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,38 +4,24 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Give numbers:");
-        
-        int num = 0;
-        int sum = 0;
-        int quan = 0;
-        int even = 0;
-        int odd = 0;
 
-        while (num != -1){
-            quan += 1;
-            sum += num;
-            num = scanner.nextInt();
+        System.out.println("Enter start digit: ");
+        int start_num = Integer.valueOf(scanner.nextLine());
 
-            if (num % 2 == 0){
-                even += 1;
-            }
-            
-            else {
-                odd += 1;
-            }
-        }
+        System.out.println("Enter end digit: ");
+        int end_num = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + (quan-1));
-        System.out.println("Average: " + (double) sum/(quan-1));
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + (odd-1));
+        divisibleByThreeInRange(start_num, end_num);
     }
 
+    public static void divisibleByThreeInRange(int num1, int num2) {
+        System.out.println("Range digits, devisible by three>>>");
+        for (int i = num1; i <= num2; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
