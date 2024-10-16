dae763b18d2d2b6a87c64a473f3897d5bcf1d82a
23DP3AKani <166131758+23DP3AKani@users.noreply.github.com>
2024-10-16 07:48:41 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,18 @@
 package lv.rvt;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class App 
 {
     public static void main( String[] args ) {
+        //Stars.printTriangle(4);
+        //Third_element();
+        Second_plus_third();
+    }
+    
+    public static void divisibleByThreeInRange_simple() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter start digit: ");
@@ -16,12 +24,46 @@ public class App
         divisibleByThreeInRange(start_num, end_num);
     }
 
-    public static void divisibleByThreeInRange(int num1, int num2) {
+    public static void divisibleByThreeInRange( int num1, int num2 ) {
         System.out.println("Range digits, devisible by three>>>");
         for (int i = num1; i <= num2; i++) {
             if (i % 3 == 0) {
                 System.out.println(i);
             }
         }
+    }
+
+    public static void Third_element() {
+
+        ArrayList<String> wordList = new ArrayList<>();
+        wordList.add("Tom");
+        wordList.add("Emma");
+        wordList.add("Alex");
+        wordList.add("Mary");
+
+        System.out.println(wordList.get(2));
+
+        wordList.remove("Alex");
+
+        System.out.println(wordList.get(2));
+
+    }    
+
+    public static void Second_plus_third() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        int num = 1;
+        System.out.println("Print a digit: ");
+        while (num != 0) {
+            num = Integer.valueOf(scanner.nextLine());
+            numbers.add(num);
+        }
+
+        int second_num = numbers.get(1);
+        int third_num = numbers.get(2);
+
+        System.out.println(second_num + third_num);
+        
     }
 }
